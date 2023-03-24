31d2949e5e01f29970d07b56b19ff7cca027276c
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-03-24 19:21:48 -0400

    Updated dependencies and wrote writeToFile func

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const markdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.error(err);
+        } else {
+            console.log("Success, the README file has been written")
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
