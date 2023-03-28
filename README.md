a1781398166c8c40da37b4f1b6f50016e14fec3b
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-03-27 21:36:52 -0400

    Started working on rendering license methods

---
+++ README.md
@@ -0,0 +1,34 @@
+
+# Readme Generator
+
+## Project Description
+This application will generate a README.md file for your current project.
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Test](#test)
+* [Questions](#questions)
+* [License](#license)
+
+## Installation
+Step 1: Run npm install and Step 2: Run node index.js
+
+## Usage
+1. Run node index.js 
+                  2.Answers the questions 
+                  3.The README.md file will be created.
+
+## Contributors
+
+
+## Test
+
+
+## Questions 
+If you have any questions, contact thossain30 on Github, or reach out via email at tamjidh99@gmail.com.
+
+## License
+Click [here](https://opensource.org/licenses/Apache-2.0) for more information about the license.
+![alt text](https://img.shields.io/badge/License-Apache%202.0-blue.svg)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,36 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch(license) {
+    case "Apache":
+      return "https://img.shields.io/badge/License-Apache%202.0-blue.svg";
+    case "MIT":
+      return "https://img.shields.io/badge/License-MIT-yellow.svg";
+    case "IBM":
+      return "https://img.shields.io/badge/License-IPL%201.0-blue.svg";
+    case "Perl":
+      return "https://img.shields.io/badge/License-Perl-0298c3.svg";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch(license) {
+    case "Apache":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+    case "IBM":
+      return "https://opensource.org/licenses/IPL-1.0";
+    case "Perl":
+      return "https://opensource.org/licenses/Artistic-2.0";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -33,13 +59,17 @@ ${data.install}
 ${data.usage}
 
 ## Contributors
-${data.usage}
+${data.contributors}
 
 ## Test
 ${data.test}
 
 ## Questions 
 If you have any questions, contact ${data.username} on Github, or reach out via email at ${data.email}.
+
+## License
+Click [here](${renderLicenseLink(data.license)}) for more information about the license.
+![alt text](${renderLicenseBadge(data.license)})
 `;
 }
 
